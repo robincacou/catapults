4b705436b0605fecb9ef00c3e518b347ca0f93c1
Robin Cassan <robin.cassan@epita.fr>
2014-11-08 19:31:18 +0100

    [TOOLSELECTOR] Made basic tool selector

---
+++ Assets/Scripts/ToolSelector.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolSelector : MonoBehaviour {
+
+	public BlockCreator blockCreator;
+	public BombGun bombGun;
+
+	ArrayList tools;
+
+	int selectedTool = 0;
+
+	void Start ()
+	{
+		tools = new ArrayList();
+		tools.Add(blockCreator);
+		tools.Add(bombGun);
+	}
+
+	void Update ()
+	{
+		if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+		{
+			selectedTool++;
+			if (selectedTool >= tools.Count)
+				selectedTool = 0;
+
+			print (selectedTool);
+
+			UpdateToolsState();
+		}
+		else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+		{
+			selectedTool--;
+			if (selectedTool < 0)
+				selectedTool = tools.Count;
+
+			print (selectedTool);
+
+			UpdateToolsState();
+		}
+	}
+
+	void UpdateToolsState()
+	{
+		for (int i = 0; i < tools.Count; ++i)
+			((MonoBehaviour)(tools[i])).enabled = (i == selectedTool);
+	}
+}
